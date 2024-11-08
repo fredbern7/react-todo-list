4d62d7c52a7e0e2973cb9e0d140ee94191384c34
Berny Fred <fredbern7@gmail.com>
2024-11-08 20:38:16 +1100

    updating

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./style.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(title) {
+    setTodos(currentTodos => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title, completed: false }
+    ]);
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos =>
+      currentTodos.map(todo =>
+        todo.id === id ? { ...todo, completed } : todo
+      )
+    );
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => currentTodos.filter(todo => todo.id !== id));
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList 
+        todos={todos} 
+        toggleTodo={toggleTodo} 
+        deleteTodo={deleteTodo}
+      />
+    </>
+  );
+}
